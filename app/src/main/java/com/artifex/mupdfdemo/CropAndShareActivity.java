d14f0ae6c8e86e980014c17efbc014577187bae9
muhammedgunes <muhammed.gunes@detaysoft.com>
2016-06-10 12:26:03 +0300

    CropAndShareActivity url null kontrolü yapıldı

--- app/src/main/java/com/artifex/mupdfdemo/CropAndShareActivity.java
+++ app/src/main/java/com/artifex/mupdfdemo/CropAndShareActivity.java
@@ -13,6 +13,7 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.answers.Answers;
@@ -76,17 +77,24 @@ public class CropAndShareActivity extends Activity {
             @Override
             public void onClick(View v) {
                 String pathofBmp = MediaStore.Images.Media.insertImage(getContentResolver(), cropImageView.getCroppedImage(),"title", null);
-                /*
-                * https://fabric.io/galepress/android/apps/ak.detaysoft.feyz/issues/5758483affcdc0425003898f
-                * Bu hata cok nadirende olsa oluyor takip edilecek crash oldugu zaman fabric-answers uzerinden tespit edilecek
-                * */
-                Answers.getInstance().logCustom(new CustomEvent("cropandShareImageUrl").putCustomAttribute("url", pathofBmp));
-                Uri bmpUri = Uri.parse(pathofBmp);
-                final Intent shareIntent = new Intent(     android.content.Intent.ACTION_SEND);
-                shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
-                shareIntent.setType("image/png");
-                startActivity(shareIntent);
+
+
+                if(pathofBmp != null) {
+                    Uri bmpUri = Uri.parse(pathofBmp);
+                    final Intent shareIntent = new Intent(     android.content.Intent.ACTION_SEND);
+                    shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+                    shareIntent.setType("image/png");
+                    startActivity(shareIntent);
+                } else {
+                    /*
+                     * https://fabric.io/galepress/android/apps/ak.detaysoft.feyz/issues/5758483affcdc0425003898f
+                     * Bu hata cok nadirende olsa oluyor takip edilecek crash oldugu zaman fabric-answers uzerinden tespit edilecek
+                     * */
+                    Answers.getInstance().logCustom(new CustomEvent("cropandShareImageUrl").putCustomAttribute("url", ""+pathofBmp));
+                    Toast.makeText(CropAndShareActivity.this, CropAndShareActivity.this.getResources().getText(R.string.WARNING_0), Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
